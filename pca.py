0ce2ea605d38940bf31cc0070e9e1177144e0a8a
Evgeny Petrov <fiddenmar@gmail.com>
2016-04-17 22:47:08 +0300

    all functions done

--- pca.py
+++ pca.py
@@ -1,41 +1,95 @@
 import sys
+inport os
 import getopt
 import os.path
 import json
+import subprocess
+import glob
+import smtplib
+from email.mime.text import MIMEText
 
 from ciscoconfparse import CiscoConfParse
 
-PCA_DEBUG_MODE = False
+def pca_load_config(pca_config_filepath):
+	if not os.path.isfile(pca_config_filepath):
+		print "Error: config file does not exist"
+		sys.exit(2)
+	
+	pca_config_file = open(pca_config_filepath, 'r')
+	pca_config = pca_config_file.read()
+	result = json.loads(pca_config)
+	return result
 
-def analyze(config, rules):
+def pca_git(pca_config):
+	pca_path = os.path.dirname(os.path.realpath(__file__))
+	if pca_config['git']['in_use'] == "yes":
+		subprocess.call(["cd", pca_config['net_config_filepath']])
+		subprocess.call(["git", "pull"])
+		subprocess.call(["cd", pca_config['user_rules_filepath']])
+		subprocess.call(["git", "pull"])
+		subprocess.call(["cd", pca_path])
+
+def pca_check_files(pca_config):
+	filelist = []
+	recorded_files = pca_config['modifications']
+	recorded_filenames = [d['filename'] for d in recorded_files]
+	recorded_timestamps = [d['timestamp'] for d in recorded_files]
+	rule_files = glob.glob("user_rules_filepath"+"/*.rule")
+	for rule_file in rule_files:
+		if rule_file in recorded_filenames:
+			target_index = recorded_filenames.index(rule_file)
+			target_timestamp = recorded_timestamps[target_index]
+			if int(target_timestamp) != os.path.getmtime(rule_file):
+				filelist = glob.glob("net_config_filepath" + "/*.config")
+	if filelist == []:
+		config_files = glob.glob("net_config_filepath" + "/*.config")
+		for config_file in config_files:
+			if config_file in recorded_filenames:
+				target_index = recorded_filenames.index(config_file)
+				target_timestamp = recorded_timestamps[target_index]
+				if int(target_timestamp) == os.path.getmtime(config_file):
+					filelist.pop(filelist.index(config_file))
+	return filelist
+
+def pca_validate(filelist, pca_config):
+	result = {"correct": True, "log":""}
+	rule_files = glob.glob("user_rules_filepath"+"/*.rule")
+	for rule_file in rule_files:
+		for config_file in filelist:
+			rules = parse_rules(rule_file)
+			config = parse_config(config_file)
+			res = pca_calidate_process(rules, config)
+			result = {result['correct'] & res['correct'], result['log'] + config_file + "\n" + res['log']}
+	return result
+	
+def pca_calidate_precess(rules, config):
 	correct = True
 	sections = rules['sections']
 	for section in sections:
 		section_type = section['type']
 		device_type = section['device_type']
-		rule = section['rule']
-		validators = section['validators']
-		config_sections = config.find_parents_w_child(section_type, rule)
-		for validator in validators:
-			valid_rule = validator['rule']
-			valid_message = validator['message']
+		trigger = section['trigger']
+		confitions = section['conditions']
+		config_sections = config.find_parents_w_child(section_type, trigger)
+		for condition in confitions:
+			valid_rule = condition['rule']
+			valid_message = condition['message']
 			valid_command = None
-			if 'command' in validator:
-				valid_command = validator['command']
+			if 'command' in condition:
+				valid_command = condition['command']
 			for config_section in config_sections:
 				res = config.find_children_w_parents(config_section, valid_rule)
-				if PCA_DEBUG_MODE:
-					print "\tsection_type: ", section_type
-					print "\trule: ", rule
-					print "\tconfig_section: ", config.find_all_children(config_section)
-					print "\tvalid_rule: ", valid_rule
-					print "\tres: ", res
-					print ""
 				if res == []:
-					print valid_message
+					log += "\tdevice_type: ", device_type + "\n"
+					log += "\tsection_type: ", section_type + "\n"
+					log += "\ttrigger: ", trigger + "\n"
+					log += "\tconfig_section: ", config.find_all_children(config_section) + "\n"
+					log += "\tvalid_rule: ", valid_rule + "\n"
+					log += "\tres: ", res + "\n"
+					log += valid_message + "\n" + "\n"
 					#do valid_command
 					correct = False
-	return correct
+	return {"correct":correct, "log":log}
 
 def parse_config(config):
 	result = CiscoConfParse(config)
@@ -47,33 +101,29 @@ def parse_rules(rules):
 	result = json.loads(rules)
 	return result
 
-def pca_run(config_filepath, rule_filepath):
-	if not os.path.isfile(config_filepath):
-		print "Error: config file does not exist"
-		sys.exit(2)
-	if not os.path.isfile(rule_filepath):
-		print "Error: rule file does not exist"
-		sys.exit(2)
+def pca_send_log(log, pca_config):
+	if pca_config['email']['in_use'] == "yes":
+	msg = MIMEText(log)
+	msg['Subject'] = 'pca report ' + datetime.datetime.now().strftime("%I:%M%p, %B %d, %Y")
+	msg['From'] = pca_config['email']['mail_from']
+	msg['To'] = pca_config['email']['mail_to']
 
-	config = parse_config(config_filepath)
-	rules = parse_rules(rule_filepath)
-
-	correct = analyze(config, rules)
-	return correct
-
+	s = smtplib.SMTP(pca_config['email']['host']+":"+pca_config['email']['port'])
+	s.starttls()
+	s.login(pca_config['email']['mail_from'], pca_config['email']['password_filepath'].read())
+	s.sendmail(pca_config['email']['mail_from'], pca_config['email']['mail_to'], msg)
+	s.quit()
 
 def print_help():
-	print "Usage: pca.py [-d] <config> <rule>"
-	print "\t-d: debug mode"
-	print "\t<config>: path to configuration file"
-	print "\t<rule>: path to custom rule file"
+	print "Usage: pca.py [<config>]"
+	print "\t<config>: .pca_config custom path (default is ~/.pca_config)"
 
 def main(argv):
-	config_filepath = ''
-	rule_filepath = ''
+
+	pca_config_filepath = "~/.pca_config"
 
 	try:
-		opts, args = getopt.getopt(argv,"hd",[])
+		opts, args = getopt.getopt(argv,"h",[])
 	except getopt.GetoptError:
 		print_help()
 		sys.exit(2)
@@ -81,21 +131,26 @@ def main(argv):
 		if opt == '-h':
 			print_help()
 			sys.exit()
-		elif opt == '-d':
-			global PCA_DEBUG_MODE
-			PCA_DEBUG_MODE = True
 		else:
 			print_help()
 			sys.exit(2)
-	if len(args) != 2:
-		print_help()
-		sys.exit(2)
 
-	config_filepath = args[0]
-	rule_filepath = args[1]
-	correct = pca_run(config_filepath, rule_filepath)
+	if len(args) == 1:
+		pca_config_filepath = args[0]
+	pca_config = pca_load_config(pca_config_filepath)
+	pca_git(pca_config)
+	filelist = pca_check_files(pca_config)
+	result = pca_validate(filelist, pca_config)
+
+	correct = result["correct"]
+	log = result["log"]
 	if not correct:
 		print "Configuration file is not correct"
+		pca_send_log(log, pca_config)
+		log_file = open(datetime.datetime.now().strftime("%I%M%p_%B%d_%Y.log"), "w")
+		text_file.write(log)
+		text_file.close()
+	pca_update_files(filelist, pca_config)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
