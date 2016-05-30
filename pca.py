f7effed7ad8de631374787482a4735cd3575aac5
Evgeny Petrov <fiddenmar@gmail.com>
2016-05-30 15:48:09 +0300

    completed

--- pca.py
+++ pca.py
@@ -12,30 +12,60 @@ from email.mime.text import MIMEText
 
 from ciscoconfparse import CiscoConfParse
 
+PCA_DEBUG = False
+PCA_VERBOSE = False
+
 def pca_load_config(pca_config_filepath):
+	global PCA_VERBOSE
+	global PCA_DEBUG
 	if not os.path.isfile(pca_config_filepath):
-		print("Error: config file does not exist")
+		print("Error: pca_config file does not exist")
 		sys.exit(2)
 	pca_config_file = open(pca_config_filepath, 'r')
+	if PCA_DEBUG:
+		print("pca_config file descriptor:")
+		print(pca_config_file)
 	pca_config = pca_config_file.read()
+	if PCA_DEBUG:
+		print("pca_config file content:")
+		print(pca_config)
 	result = json.loads(pca_config)
+	if PCA_DEBUG:
+		print("pca_config JSON structure:")
+		print(result)
 	return result
 
 def pca_git(pca_config):
+	global PCA_VERBOSE
+	global PCA_DEBUG
 	user_rules_filepath = os.path.expanduser(pca_config['user_rules_filepath'])
 	net_config_filepath = os.path.expanduser(pca_config['net_config_filepath'])
+	if PCA_DEBUG:
+		print("user rules filepath: " + user_rules_filepath)
+		print("net config filepath: " + net_config_filepath)
+	if PCA_DEBUG:
+		print("git usage: " + pca_config['git']['in_use'])
 	if pca_config['git']['in_use'] == "yes":
+		if PCA_VERBOSE:
+			print("updating git repositories")
 		subprocess.call(["git", "pull"], cwd=net_config_filepath)
 		subprocess.call(["git", "pull"], cwd=user_rules_filepath)
 
 def pca_check_files(pca_config):
+	global PCA_VERBOSE
+	global PCA_DEBUG
 	filelist = []
 	recorded_files = pca_config['modifications']
 	user_rules_filepath = os.path.expanduser(pca_config['user_rules_filepath'])
 	net_config_filepath = os.path.expanduser(pca_config['net_config_filepath'])
 	recorded_filenames = [d['filename'] for d in recorded_files]
 	recorded_timestamps = [d['timestamp'] for d in recorded_files]
+	if PCA_VERBOSE:
+		print("checking rule files")
 	rule_files = glob.glob(user_rules_filepath+"/*.rule")
+	if PCA_DEBUG:
+		print("rule files: ")
+		print(rule_files)
 	for rule_file in rule_files:
 		if rule_file in recorded_filenames:
 			target_index = recorded_filenames.index(rule_file)
@@ -43,28 +73,45 @@ def pca_check_files(pca_config):
 			if int(target_timestamp) != int(os.path.getmtime(rule_file)):
 				filelist = glob.glob(net_config_filepath + "/*.config")
 	if filelist == []:
+		if PCA_VERBOSE:
+			print("checking config files")
 		filelist = glob.glob(net_config_filepath + "/*.config")
 		config_files = glob.glob(net_config_filepath + "/*.config")
+		if PCA_DEBUG:
+			print("config files: ")
+			print(config_files)
 		for config_file in config_files:
 			if config_file in recorded_filenames:
 				target_index = recorded_filenames.index(config_file)
 				target_timestamp = recorded_timestamps[target_index]
 				if int(target_timestamp) == int(os.path.getmtime(config_file)):
 					filelist.pop(filelist.index(config_file))
+	if PCA_DEBUG:
+		print("resulting filelist:")
+		print(filelist)
 	return filelist
 
-def pca_validate(filelist, pca_config):
+def pca_verify(filelist, pca_config):
+	global PCA_VERBOSE
+	global PCA_DEBUG
 	result = {"correct": True, "log":""}
 	filelist_incorrect = []
 	user_rules_filepath = os.path.expanduser(pca_config['user_rules_filepath'])
 	rule_files = glob.glob(user_rules_filepath+"/*.rule")
 	for rule_file in rule_files:
 		for config_file in filelist:
+			if PCA_VERBOSE:
+				print("checking " + config_file + " using rules from " + rule_file)
 			rules = parse_rules(rule_file)
 			config = parse_config(config_file)
-			res = pca_validate_process(rules, config)
+			res = pca_verify_process(rules, config)
 			correct = result['correct'] & res['correct']
-			log = result['log']
+			log = res['log']
+			if PCA_DEBUG:
+				print("config: " + config_file)
+				print("rules: " + rule_file)
+				print("result: ")
+				print(res)
 			if not res['correct']:
 				filelist_incorrect.append(config_file)
 				log += config_file + "\n" + res['log']
@@ -73,7 +120,9 @@ def pca_validate(filelist, pca_config):
 		filelist.pop(filelist.index(file_incorrect))
 	return result
 	
-def pca_validate_process(rules, config):
+def pca_verify_process(rules, config):
+	global PCA_VERBOSE
+	global PCA_DEBUG
 	correct = True
 	log = ""
 	sections = rules['sections']
@@ -103,17 +152,29 @@ def pca_validate_process(rules, config):
 	return {"correct":correct, "log":log}
 
 def parse_config(config):
+	if not os.path.isfile(config):
+		print("Error: config file does not exist")
+		sys.exit(2)
 	result = CiscoConfParse(config)
 	return result
 
 def parse_rules(rules):
+	if not os.path.isfile(rules):
+		print("Error: rule file does not exist")
+		sys.exit(2)
 	rule_file = open(rules, 'r')
 	rules = rule_file.read()
 	result = json.loads(rules)
 	return result
 
 def pca_send_log(log, pca_config):
+	global PCA_VERBOSE
+	global PCA_DEBUG
+	if PCA_DEBUG:
+		print("email usage: " + pca_config['email']['in_use'])
 	if pca_config['email']['in_use'] == "yes":
+		if PCA_VERBOSE:
+			print("sending mail")
 		msg = MIMEText(log)
 		msg['Subject'] = 'pca report ' + datetime.datetime.now().strftime("%I:%M%p, %B %d, %Y")
 		msg['From'] = pca_config['email']['mail_from']
@@ -131,12 +192,16 @@ def search(ld, name):
 	return None
 
 def pca_update_files(filelist, pca_config, pca_config_filepath):
+	global PCA_VERBOSE
+	global PCA_DEBUG
 	user_rules_filepath = os.path.expanduser(pca_config['user_rules_filepath'])
 	net_config_filepath = os.path.expanduser(pca_config['net_config_filepath'])
 	modifications = pca_config['modifications']
 	recorded_filenames = [d['filename'] for d in modifications]
 	recorded_timestamps = [d['timestamp'] for d in modifications]
 	for config_file in filelist:
+		if PCA_VERBOSE:
+			print("updating config file "+config_file+" information")
 		if config_file in recorded_filenames:
 			f = search(modifications, config_file)
 			modifications[modifications.index(f)]['timestamp'] = os.path.getmtime(config_file)
@@ -144,6 +209,8 @@ def pca_update_files(filelist, pca_config, pca_config_filepath):
 			modifications.append({'filename':config_file, 'timestamp':os.path.getmtime(config_file)})
 	rule_files = glob.glob(user_rules_filepath+"/*.rule")
 	for rule_file in rule_files:
+		if PCA_VERBOSE:
+			print("updating rule file "+rule_file+" information")
 		if rule_file in recorded_filenames:
 			f = search(modifications, rule_file)
 			modifications[modifications.index(f)]['timestamp'] = os.path.getmtime(rule_file)
@@ -154,37 +221,87 @@ def pca_update_files(filelist, pca_config, pca_config_filepath):
 	json.dump(pca_config, outfile)
 
 def print_help():
-	print("Usage: pca.py [<config>]")
-	print("\t<config>: .pca_config custom path (default is ~/.pca_config)")
+	print("Usage: pca.py [-h][-d][-v][-c <config>][-s <filepath>]")
+	print("\t-h, --help: print out help")
+	print("\t-d, --debug: debug details")
+	print("\t-v, --verbose: additional details about processes")
+	print("\t-c, --config: .pca_config custom path (default is ~/.pca_config)")
+	print("\t-s, --save: log save filepath")
 
 def main(argv):
+	global PCA_VERBOSE
+	global PCA_DEBUG
 	pca_config_filepath = os.path.expanduser("~/.pca_config")
+	pca_log_filepath = ("./")
 	try:
-		opts, args = getopt.getopt(argv,"h",[])
+		opts, args = getopt.getopt(argv,"hdvc:s:",['help', 'debug', 'verbose', 'custom', 'save'])
 	except getopt.GetoptError:
 		print_help()
 		sys.exit(2)
 	for opt, arg in opts:
-		if opt == '-h':
+		if opt in ('-h', '--help'):
 			print_help()
 			sys.exit()
+		elif opt in ('-d', '--debug'):
+			PCA_DEBUG = True
+		elif opt in ('-v', '--verbose'):
+			PCA_VERBOSE = True
+		elif opt in ('-c', '--custom'):
+			pca_config_filepath = os.path.expanduser(arg)
+		elif opt in ('-s', '--save'):
+			pca_log_filepath = os.path.expanduser(arg)
 		else:
 			print_help()
 			sys.exit(2)
-	if len(args) == 1:
-		pca_config_filepath = args[0]
+	if PCA_DEBUG:
+		print("pca_config filepath: " + pca_config_filepath)
+		print("log save filepath: " + pca_log_filepath)
+	if PCA_VERBOSE:
+		print("loading pca_config")
 	pca_config = pca_load_config(pca_config_filepath)
-	pca_git(pca_config)
+	if pca_config == None:
+		print("Error: pca_config file is invalid")
+		sys.exit(2)
+	if PCA_VERBOSE:
+		print("pca_config loaded")
+	if PCA_VERBOSE:
+		print("checking git")
+	pca_git_result = pca_git(pca_config)
+	if PCA_VERBOSE:
+		print("git checked")
+	if PCA_VERBOSE:
+		print("checking files")
 	filelist = pca_check_files(pca_config)
-	result = pca_validate(filelist, pca_config)
+	if PCA_VERBOSE:
+		print("files checked")
+	if PCA_VERBOSE:
+		print("verifying")
+	result = pca_verify(filelist, pca_config)
+	if PCA_VERBOSE:
+		print("verifying completed")
+	if PCA_DEBUG:
+		print("verify results:")
+		print(result)
 	correct = result["correct"]
 	log = result["log"]
 	if not correct:
+		if PCA_VERBOSE:
+			print("checking email")
 		pca_send_log(log, pca_config)
-		log_file = open(datetime.datetime.now().strftime("%I%M%p_%B%d_%Y.log"), "w")
+		if PCA_VERBOSE:
+			print("email checked")
+		if PCA_VERBOSE:
+			print("saving log")
+		log_file = open(pca_log_filepath + datetime.datetime.now().strftime("%I%M%p_%B%d_%Y.log"), "w")
 		log_file.write(log)
 		log_file.close()
+		if PCA_VERBOSE:
+			print("log saved")
+	if PCA_VERBOSE:
+		print("updating file information")
 	pca_update_files(filelist, pca_config, pca_config_filepath)
+	if PCA_VERBOSE:
+		print("file information updated")
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
--- pca_test.py
+++ pca_test.py
@@ -1,31 +1,136 @@
-def pca_test_load_config():
+import pca
+
+def pca_test_load_config(): 
 	return pca_test_load_config_exist_true() &&
-	pca_test_load_config_exist_false() &&
+	not pca_test_load_config_exist_false() &&
 	pca_test_load_config_json_true() &&
-	pca_test_load_config_json_false() &&
+	not pca_test_load_config_json_false() &&
 	pca_test_load_config_empty_true() &&
-	pca_test_load_config_empty_false()
+	not pca_test_load_config_empty_false()
 
-def pca_test_load_config_verbose():
-	print ("pca_test_load_config_exist_true():", pca_test_load_config_exist_true()
-	print ("pca_test_load_config_exist_false():", pca_test_load_config_exist_false()
-	print ("pca_test_load_config_json_true():", pca_test_load_config_json_true()
-	print ("pca_test_load_config_json_false():", pca_test_load_config_json_false()
-	print ("pca_test_load_config_empty_true():", pca_test_load_config_empty_true()
-	print ("pca_test_load_config_empty_false():", pca_test_load_config_empty_false()
+def pca_test_load_config_verbose(): 
+	print ("pca_test_load_config_exist_true(): ", pca_test_load_config_exist_true() )
+	print ("pca_test_load_config_exist_false(): ", not pca_test_load_config_exist_false() )
+	print ("pca_test_load_config_json_true(): ", pca_test_load_config_json_true() )
+	print ("pca_test_load_config_json_false(): ", not pca_test_load_config_json_false() )
+	print ("pca_test_load_config_empty_true(): ", pca_test_load_config_empty_true() )
+	print ("pca_test_load_config_empty_false(): ", not pca_test_load_config_empty_false() )
 
-def pca_test_git():
+def pca_test_git(): 
 	return pca_test_git_use_true() &&
-	pca_test_git_use_false() &&
+	not pca_test_git_use_false() &&
 	pca_test_git_path_true() &&
-	pca_test_git_path_false() &&
+	vpca_test_git_path_false() &&
 	pca_test_git_repo_true() &&
-	pca_test_git_repo_false()
+	not pca_test_git_repo_false()
 
-def pca_test_git_verbose():
-	print ("pca_test_git_use_true():", pca_test_git_use_true()
-	print ("pca_test_git_use_false():", pca_test_git_use_false()
-	print ("pca_test_git_path_true():", pca_test_git_path_true()
-	print ("pca_test_git_path_false():", pca_test_git_path_false()
-	print ("pca_test_git_repo_true():", pca_test_git_repo_true()
-	print ("pca_test_git_repo_false():", pca_test_git_repo_false()
+def pca_test_git_verbose(): 
+	print ("pca_test_git_use_true(): ", pca_test_git_use_true() )
+	print ("pca_test_git_use_false(): ", not pca_test_git_use_false() )
+	print ("pca_test_git_path_true(): ", pca_test_git_path_true() )
+	print ("pca_test_git_path_false(): ", not pca_test_git_path_false() )
+	print ("pca_test_git_repo_true(): ", pca_test_git_repo_true() )
+	print ("pca_test_git_repo_false(): ", not pca_test_git_repo_false() )
+
+def pca_test_check_files():
+	return pca_test_check_files_exist_true() &&
+	not pca_test_check_files_exist_false() &&
+	pca_test_check_files_modified_true() &&
+	not pca_test_check_files_modified_false()
+
+def pca_test_check_files_verbose():
+	print("pca_test_check_files_exist_true(): ", pca_test_check_files_exist_true() )
+	print("pca_test_check_files_exist_false(): ", not pca_test_check_files_exist_false() )
+	print("pca_test_check_files_modified_true(): ", pca_test_check_files_modified_true() )
+	print("pca_test_check_files_modified_false(): ", not pca_test_check_files_modified_false() )
+
+def pca_test_verify_process():
+	return pca_test_verify_process_exist_true() &&
+	not pca_test_verify_process_exist_false &&
+	pca_test_verify_process_correct_true &&
+	not pca_test_verify_process_correct_false
+
+def pca_test_verify_process_verbose():
+	print("pca_test_verify_process_exist_true(): ", pca_test_verify_process_exist_true() )
+	print("pca_test_verify_process_exist_false: ", not pca_test_verify_process_exist_false )
+	print("pca_test_verify_process_correct_true: ", pca_test_verify_process_correct_true )
+	print("pca_test_verify_process_correct_false: ", not pca_test_verify_process_correct_false )
+
+def pca_test_parse_config():
+	return pca_test_parse_config_exist_true() &&
+	not pca_test_parse_config_exist_false() &&
+	pca_test_parse_config_correct_true() &&
+	not pca_test_parse_config_correct_false()
+
+def pca_test_parse_config_verbose():
+	print("pca_test_parse_config_exist_true(): ", pca_test_parse_config_exist_true() )
+	print("pca_test_parse_config_exist_false(): ", not pca_test_parse_config_exist_false() )
+	print("pca_test_parse_config_correct_true(): ", pca_test_parse_config_correct_true() )
+	print("pca_test_parse_config_correct_false(): ", not pca_test_parse_config_correct_false() )
+
+def pca_test_parse_rules():
+	return pca_test_parse_config_exist_true() &&
+	not pca_test_parse_config_exist_false() &&
+	pca_test_parse_config_correct_true() &&
+	not pca_test_parse_config_correct_false()
+
+def pca_test_parse_rules_verbose():
+	print("pca_test_parse_rules_exist_true(): ", pca_test_parse_rules_exist_true() )
+	print("pca_test_parse_rules_exist_false(): ", not pca_test_parse_rules_exist_false() )
+	print("pca_test_parse_rules_correct_true(): ", pca_test_parse_rules_correct_true() )
+	print("pca_test_parse_rules_correct_false(): ", not pca_test_parse_rules_correct_false() )
+
+def pca_test_send_log():
+	return pca_test_send_log_use_true() &&
+	not pca_test_send_log_use_false() &&
+	pca_test_send_log_mail_true() &&
+	not pca_test_send_log_mail_false()
+
+def pca_test_send_log_verbose():
+	print("pca_test_send_log_use_true(): ", pca_test_send_log_use_true() )
+	print("pca_test_send_log_use_false(): ", not pca_test_send_log_use_false() )
+	print("pca_test_send_log_mail_true(): ", pca_test_send_log_mail_true() )
+	print("pca_test_send_log_mail_false(): ", not pca_test_send_log_mail_false() )
+
+def pca_test_update_files():
+	return pca_test_update_files_changes_true() &&
+	pca_test_update_files_changes_false()
+
+def pca_test_update_files_verbose():
+	print("pca_test_update_files_changes_true(): ", pca_test_update_files_changes_true() )
+	print("pca_test_update_files_changes_false(): ", pca_test_update_files_changes_false() )
+
+def pca_test_all():
+	return pca_test_load_config() &&
+	pca_test_git() &&
+	pca_test_check_files() &&
+	pca_test_verify_process() &&
+	pca_test_parse_config() &&
+	pca_test_parse_rules() &&
+	pca_test_send_log() &&
+	pca_test_update_files()
+
+def pca_test_all_verbose():
+	print("pca_test_load_config(): ", pca_test_load_config() )
+	pca_test_load_config_verbose()
+	print("pca_test_git(): ", pca_test_git() )
+	pca_test_git_verbose()
+	print("pca_test_check_files(): ", pca_test_check_files() )
+	pca_test_check_files_verbose()
+	print("pca_test_verify_process(): ", pca_test_verify_process() )
+	pca_test_verify_process_verbose()
+	print("pca_test_parse_config(): ", pca_test_parse_config() )
+	pca_test_parse_config_verbose()
+	print("pca_test_parse_rules(): ", pca_test_parse_rules() )
+	pca_test_parse_rules_verbose()
+	print("pca_test_send_log(): ", pca_test_send_log() )
+	pca_test_send_log_verbose()
+	print("pca_test_update_files(): ", pca_test_update_files() )
+	pca_test_update_files_verbose()
+
+def main(argv):
+	if pca_test_all() != True:
+		pca_test_all_verbose()
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
